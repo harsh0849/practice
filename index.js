200b1897e48f6aa80120154055e9287f809c47f4
harsh0849 <harsh.evdt@gmail.com>
2025-01-03 16:59:59 +0530

    new route

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const port = 3000
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+app.get('/about', (req, res) => {
+  res.send('its an about page..')
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
